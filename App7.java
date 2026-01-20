e37e5be8024288349211b73dceac402dd331d2ce
Lucas Pereira Fernandes <lucasperfer83@gmail.com>
2026-01-20 01:01:49 -0300

    Add files via upload

---
+++ App7.java
@@ -0,0 +1,27 @@
+package logging;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.event.Level;
+
+public class App7 {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger("app7");
+
+    public static void main(String[] args) {
+
+        if (LOGGER.isInfoEnabled()){
+            LOGGER.info("Result: {}", calculate());
+        }
+
+    }
+    private static int calculate(){
+        System.out.println("Calculating...");
+        try{
+            Thread.sleep(2000);
+        }catch (InterruptedException e){
+            throw new RuntimeException(e);
+        }
+        return 10;
+    }
+}
